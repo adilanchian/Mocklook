375a62b42346001db5a00f205c4eaa95b72aaf4c
Alec Dilanchian <alec.dilanchian@gmail.com>
2018-03-05 23:34:09 -0600

    Adding unit tests for CalendarManager issue #8

--- Mocklook/MocklookTests/MocklookTests.swift
+++ Mocklook/MocklookTests/MocklookTests.swift
@@ -10,7 +10,9 @@ import XCTest
 
 class MocklookTests: XCTestCase {
     //-- Properties: Calendar Manager --//
-    var calendarManager = CalendarManager()
+    let calendarManager = CalendarManager()
+    let stringDate = "Monday, March 5"
+    let dayPath = IndexPath(row: 20, section: 11) // 12/19/2018
     
     override func setUp() {
         super.setUp()
@@ -22,6 +24,7 @@ class MocklookTests: XCTestCase {
         super.tearDown()
     }
     
+    //-- Calendar Manager Tests --//
     func testCalendarManager_numDaysByMonth() {
         print("Testing numDaysByMonth...")
         
@@ -30,7 +33,7 @@ class MocklookTests: XCTestCase {
         print("[numDaysByMonthTest] numDays: \(numDays)")
         
         guard let numDaysTest = Calendar.current.range(of: .day, in: .month, for: Date()) else {
-            print("[numDaysByMonthTest] Failed. numDaysTest was nil")
+            print("[numDaysByMonthTest] Failed. numDaysTest was nil.")
             assert(false)
         }
         
@@ -39,6 +42,52 @@ class MocklookTests: XCTestCase {
         if numDays == numDaysTest.count {
             print("[numDaysByMonthTest] Passed!")
             assert(true)
+        }
+    }
+    
+    func testCalendarManager_getEachDayForYear() {
+        print("Testing getEachDayForYear...")
+        
+        self.calendarManager.getEachDayForYear()
+        
+        // There should be 365 section for the year of 2018 //
+        if self.calendarManager.sectionDays.count == 365 {
+            print("[getEachDayForYear] Passed!")
+            assert(true)
+        } else {
+            print("[getEachDayForYear] Failed. Count: \(self.calendarManager.sectionDays.count)")
+            assert(false)
+        }
+    }
+    
+    func testCalendarManager_calculateCalendarPath() {
+        print("Testing calculateCalendarPath...")
+        
+        guard let path = self.calendarManager.calculateCalendarPath(stringDate: self.stringDate) else {
+            print("[calculateCalendarPath] Failed. Invalid date.")
+            assert(false)
+        }
+        
+        if path == IndexPath(row: 4, section: 2) {
+            print("[calculateCalendarPath] Passed!")
+            assert(true)
+        } else {
+            print("[calculateCalendarPath] Failed. IndexPath: \(path).")
+            assert(false)
+        }
+    }
+    
+    func testCalendarManager_calculateAgendaPath() {
+        print("Testing calculateAgendaPath...")
+        
+        let path = self.calendarManager.calculateAgendaPath(dayPath: dayPath)
+        
+        if path == IndexPath(row: 0, section: 354) {
+            print("[calculateAgendaPath] Passed!")
+            assert(true)
+        } else {
+            print("[calculateAgendaPath] Failed. IndexPath: \(path).")
+            assert(false)
         }
     }
 }
